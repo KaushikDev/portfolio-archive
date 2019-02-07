dbf3e76183bb1599c9bc7dae605ce92d27295de4
kaushikDev <piyush.kaushik02@gmail.com>
2019-02-08 02:59:45 +0530

    saved build

--- logic.js
+++ logic.js
@@ -1,31 +1,21 @@
-  
- //==============================================//
- 
- 
- $(document).ready(function () {
-                $("a").addClass("animated bounce");
-                $("img").addClass("animated swing");
-            });
-		var email;
-function postThanks(){
-email = document.getElementById("MERGE0").value;
-if(!checkEmail(email)){
-//alert("Please enter your email to subscribe");
-document.getElementById("postThanks").innerHTML = "Please enter valid email to subscribe";
-document.getElementById("postThanks").style.color = "red";	
-	return false;
-}
-else
-onValidEmail();
-}
-function onValidEmail(){
-//name = document.getElementById("MERGE0").value;
-//document.getElementById("postThanks").innerHTML = "";
-document.getElementById("postThanks").innerHTML = "Thanks for subscribing with \""+email+"\". We will keep you posted for new/enhanced apps.";
-document.getElementById("postThanks").style.color = "green";		
-document.getElementById("form").action = "https://twitter.us16.list-manage.com/subscribe/post";	
-}
-function checkEmail(email){
-  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(email);
-}
+	var email;
+				
+		function postThanks(){
+			email = document.getElementById("MERGE0").value;
+			if(!checkEmail(email)){
+				alert("Please enter valid email to subscribe");	
+				return false;
+			}
+			else
+				onValidEmail();
+		}
+		
+		function onValidEmail(){
+			alert("Thanks for subscribing with us. We will keep you posted. ");
+		}
+		
+		
+		function checkEmail(email){
+		  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		  return re.test(email);
+		}
